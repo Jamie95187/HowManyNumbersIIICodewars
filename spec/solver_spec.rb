49d8b423fc2aa16b2a940046138b7db0e3fa8e30
Jamie <jamiewong63@googlemail.com>
2020-07-10 21:24:55 +0100

    test input: 5,3 output: [1,113,113]

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -20,6 +20,7 @@ describe 'solver' do
     it('should return the correct answer when the amount of digits is 3') do
       expect(solver.find_all(3, 3)).to eq [1, 111, 111]
       expect(solver.find_all(4, 3)).to eq [1, 112, 112]
+      expect(solver.find_all(5, 3)).to eq [1, 113, 113]
     end
 
   end
