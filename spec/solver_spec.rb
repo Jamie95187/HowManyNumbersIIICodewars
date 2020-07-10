ac4b1e7c5ad3c8c0bfdaac2233d708de4c1c4de9
Jamie <jamiewong63@googlemail.com>
2020-07-10 21:05:58 +0100

    test input: (6,2) output: [2,15,33]

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -11,6 +11,7 @@ describe 'solver' do
       expect(solver.find_all(3, 2)).to eq [1, 12, 12]
       expect(solver.find_all(4, 2)).to eq [2, 13, 22]
       expect(solver.find_all(5, 2)).to eq [2, 14, 23]
+      expect(solver.find_all(6, 2)).to eq [2, 15, 33]
     end
 
   end
