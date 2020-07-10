44e19fcf6c80935e4760bf3000596f5f20c7144c
Jamie <jamiewong63@googlemail.com>
2020-07-10 20:53:03 +0100

    test input: (4,2) output: [2,13,22]

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,8 +7,9 @@ describe 'solver' do
   describe 'find_all' do
 
     it('should return the correct answer when the amount of digits is 2') do
-      expect(solver.find_all(4, 2)).to eq [3, 13, 31]
-      expect(solver.find_all(6, 2)).to eq [5, 15, 51]
+      # expect(solver.find_all(2, 2)).to eq [5, 15, 51]
+      expect(solver.find_all(4, 2)).to eq [2, 13, 22]
+      # expect(solver.find_all(6, 2)).to eq [5, 15, 22]
     end
 
   end
