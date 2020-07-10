cd4852fb2831555d490ef974231f8d8e55454b31
Jamie <jamiewong63@googlemail.com>
2020-07-10 23:01:31 +0100

    test input: 7,3 output: [5,115,331]

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -21,7 +21,8 @@ describe 'solver' do
       expect(solver.find_all(3, 3)).to eq [1, 111, 111]
       expect(solver.find_all(4, 3)).to eq [1, 112, 112]
       expect(solver.find_all(5, 3)).to eq [1, 113, 113]
-      expect(solver.find_all(6, 3)).to eq [3, 114, 211]
+      expect(solver.find_all(6, 3)).to eq [4, 114, 321]
+      expect(solver.find_all(7, 3)).to eq [5, 115, 331]
     end
 
   end
